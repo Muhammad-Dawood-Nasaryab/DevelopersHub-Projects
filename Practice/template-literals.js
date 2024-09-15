ffa4bd2e63caef70abb8db49caa3562baa1632a2
CreativeDreamer1 <mdawoodnasaryab@gmail.com>
2024-09-15 19:19:55 +0500

    template literals

---
+++ Practice/template-literals.js
@@ -0,0 +1,14 @@
+function highlight(str, ...values) {
+    return str.reduce((bef, aft, i) => {
+        return (`${bef} <b>${values[i - 1]}</b> ${aft}`)
+    })
+}
+
+const nam = "Dawood";
+const age = 16;
+const city = "Lahore";
+
+console.log(highlight`My name is ${nam}, I am ${age} years old and I live in ${city}.`);
+
+const escaped = `Here is a backtick: \``;
+console.log(escaped);
