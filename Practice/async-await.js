8dbae667a823ad32564829e6140faddd82cd9df0
CreativeDreamer1 <mdawoodnasaryab@gmail.com>
2024-09-15 18:56:48 +0500

    async-await

--- Practice/async-await.js
+++ Practice/async-await.js
@@ -1,4 +1,15 @@
-// function divide(n1, n2) { return n1/n2 };
+function divide(n1, n2) { return n1/n2 };
+
+const promise = new Promise((resolve, reject) => {
+    let num1 = 2;
+    let num2 = 4;
+    resolve (divide(num1, num2));
+});
+
+promise.then(
+    function (value) { value },
+    function (error) { console.error('Error:', error) }
+);
 
 async function myFun () {
     try {
